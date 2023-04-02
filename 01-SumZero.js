460062a5a88129426087ce22357c8b1b181720f9
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-04-02 11:24:56 +0530

    Update 01-SumZero.js

--- 01-SumZero.js
+++ 01-SumZero.js
@@ -14,3 +14,26 @@ function getnumber(array){
 
  const result=getnumber([-5, -4, -3 ,-2, 0 , 2,4,6,8]);
  console.log(result);
+
+OR //to reduce time complexity
+
+
+function getArray(array){
+    let left=0;
+    let right=array.length-1;
+
+    while(left<right){
+        sum= array[left]+array[right];
+        if(sum==0){
+                return [array[left],array[right]];
+        }
+        else if(sum>0){
+            right--;
+        }
+        else {
+            left++;
+        }
+    }
+ }
+ const result=getArray([-5, -4, -3 ,-2, 0 , 2,4,6,8]);
+ console.log(result);
