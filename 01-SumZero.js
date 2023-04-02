0a9ef24dc59edd0c8b29ae5a090b26e7c17424da
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-04-02 10:36:04 +0530

    Create 01-SumZero.js

---
+++ 01-SumZero.js
@@ -0,0 +1,16 @@
+///checking sum zero - problem 1
+// [-5, -4, -3 ,-2, 0 , 2,4,6,8] ->input
+// [-4,4] ->output;
+
+function getnumber(array){
+    for(let number of array){
+        for(let j=1;j<array.lenght ; j++){
+            if(number + array[j] ===0){
+                return[number , array[j]];
+            }
+        }
+    }
+}
+
+ const result=getnumber([-5, -4, -3 ,-2, 0 , 2,4,6,8]);
+ console.log(result);
