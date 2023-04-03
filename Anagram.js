532269c985066d96a1a40786b491d351d9c4068e
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-04-03 18:46:59 +0530

    Create Anagram.js

---
+++ Anagram.js
@@ -0,0 +1,21 @@
+function anagram(str1 ,str2){
+  if(str1.lenght!==str2.lenght){
+    return false;
+  }
+  let count={};
+  for(let letter of str1){
+    count[letter]=(count[letter] || 0) + 1;
+    console.log(count[letter]);
+  } 
+
+  for(let items of str2){
+    if(!count[items]){
+      return false;
+    }
+    count[items]-=1.
+  }
+  return true;
+}
+ const check=anagram('hello' , 'llheo');
+ console.log(check);
+
