32809133b9658f3a09fc54e053391e07430314ca
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-04-03 19:11:51 +0530

    Create Unique.js

---
+++ Unique.js
@@ -0,0 +1,25 @@
+//Count Unique Numbers
+//
+//out put-> 8
+//condition
+//i=0,j=0
+//(array[i]!==array[j]
+// 1. i+1
+// 2.  array[i]=array[j]
+function uni(array){
+    if(array.length>0){
+        let i=0;
+        for(let j=1;j<array.length;j++){
+            if(array[i]!==array[j]){
+            i++;
+            array[i]=array[j];
+            }
+        }
+        return i+1;
+    }
+    else{
+        throw new Error("array is Empty");
+    }
+}
+const nnn=uni([1,1,2,2,3,4,4,5,6,7,8,8]);
+console.log(nnn);
