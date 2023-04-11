0cee1232385573822e7b8c1f33cb67158e318933
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-04-11 20:11:06 +0530

    Create Array.js

---
+++ Array.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <script>
+       let arr = [10 ,100,88,54,56,47,78,99,12,1,2,5];
+       // for(let i=0 ; i<arr.length;i++){
+       //     document.write(arr[i]);
+       // } -->
+        let x=4;
+        document.write(arr[x]);
+    </script>
+</head>
+<body>
+    
+</body>
+</html>
